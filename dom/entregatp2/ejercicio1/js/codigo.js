b3569c66eb7c68c42043d908382aa4b1964828be
AbelBerges <abel.berges@gmail.com>
2024-02-27 18:43:51 -0300

    Faltaba una validación

--- dom/entregatp2/ejercicio1/js/codigo.js
+++ dom/entregatp2/ejercicio1/js/codigo.js
@@ -7,12 +7,16 @@ function valido(){
     let num = document.forms["primero"]["otro"].value;
     //let num = document.getElementById("otro").value;
     //console.log("Muestro el num "+num);
+    let control = false;
     patron = /^(\d){1,3$/;
     let mensaje = '';
     if(num >= 1 && num <= 100){
         mensaje = "<span class='correcto'>El número ingresado está dentro de el conjunto requerido</span>";
+        document.getElementById("salida").innerHTML = mensaje;
+        control = true;
     } else {
         mensaje = "<span class='fallo'>El número ingresado no está dento de el conjunto requerido</span>";
+        document.getElementById("salida").innerHTML = mensaje;
     }
-    return document.getElementById("salida").innerHTML = mensaje;
+    return control;
 }
